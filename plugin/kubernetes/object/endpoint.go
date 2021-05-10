0a3375e76bba0688ce479bcdb34d4b9ae524a438
Chris O'Haver <cohaver@infoblox.com>
2021-05-10 13:29:56 -0400

    copy unready exclusion to v1beta1 func (#4616)
    Signed-off-by: Chris O'Haver <cohaver@infoblox.com>

--- plugin/kubernetes/object/endpoint.go
+++ plugin/kubernetes/object/endpoint.go
@@ -178,6 +178,9 @@ func EndpointSliceV1beta1ToEndpoints(obj meta.Object) (meta.Object, error) {
 	}
 
 	for _, end := range ends.Endpoints {
+		if end.Conditions.Ready == nil || !*end.Conditions.Ready {
+			continue
+		}
 		for _, a := range end.Addresses {
 			ea := EndpointAddress{IP: a}
 			if end.Hostname != nil {
