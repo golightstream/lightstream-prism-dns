9b94696b115d2d1394388e2b15c8ff05e5273cdf
Ben Kaplan <77549469+kaplanben@users.noreply.github.com>
2022-12-01 16:07:13 +0200

    plugin/edns: remove truncating of question section on bad EDNS version (#5787)
    * plugin/edns: remove truncating of question section on bad EDNS version
    
    EDNS requests of "Unknown Version" removed the query section altogether.
    Not sure why since this is not require (see [link](https://kb.isc.org/docs/edns-compatibility-dig-queries)
    
    This cause issues with DNS solutions that uses this information (initial queried name, type and class) in order to route the response to the right client (e.g. PDNS).
    
    The change here is to keep the inital query section as is.
    
    Signed-off-by: Ben Kaplan <ben.kaplan@redis.com>
    
    * adding tests for edns0 version check
    
    Signed-off-by: Ben Kaplan <ben.kaplan@redis.com>
    
    * adding tests for non-edns0 version check
    
    Signed-off-by: Ben Kaplan <ben.kaplan@redis.com>
    
    Signed-off-by: Ben Kaplan <ben.kaplan@redis.com>

--- plugin/pkg/edns/edns_test.go
+++ plugin/pkg/edns/edns_test.go
@@ -10,9 +10,18 @@ func TestVersion(t *testing.T) {
 	m := ednsMsg()
 	m.Extra[0].(*dns.OPT).SetVersion(2)
 
-	_, err := Version(m)
+	r, err := Version(m)
 	if err == nil {
 		t.Errorf("Expected wrong version, but got OK")
+	}
+	if r.Question == nil {
+		t.Errorf("Expected question section, but got nil")
+	}
+	if r.Rcode != dns.RcodeBadVers {
+		t.Errorf("Expected Rcode to be of BADVER (16), but got %d", r.Rcode)
+	}
+	if r.Extra == nil {
+		t.Errorf("Expected OPT section, but got nil")
 	}
 }
 
@@ -20,9 +29,12 @@ func TestVersionNoEdns(t *testing.T) {
 	m := ednsMsg()
 	m.Extra = nil
 
-	_, err := Version(m)
+	r, err := Version(m)
 	if err != nil {
 		t.Errorf("Expected no error, but got one: %s", err)
+	}
+	if r != nil {
+		t.Errorf("Expected nil since not an EDNS0 request, but did not got nil")
 	}
 }
 
